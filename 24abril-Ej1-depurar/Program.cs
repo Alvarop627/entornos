01c55b37fb7e744e6efe34843d78855672ee5d46
Álvaro Reina <alvarop627@gmail.com>
2019-04-24 19:53:30 +0200

    Agregar archivos de proyecto.

---
+++ 24abril-Ej1-depurar/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error
+            // Use a switch statement to do the math
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                case "c":
+
+                    if (num1 >= num2)
+                    {
+                        result = num1*num1;
+                    }
+                    else { result = num2 * num2; }
+
+                    
+                    break;
+                // Return text for an incorrect option entry
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            // Display title as the C# console calculator app
+            Console.WriteLine("Calculadora en consola en C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                // Declare variables and set to empty
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                // Ask the user to type the first number
+                Console.Write("Escribe un numero,despues pulsa Intro: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Esta no es uan entrada valida. Por favor, escribe un numero entero: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number
+                Console.Write("Escribe otro numero,despues pulsa Intro: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Esta no es uan entrada valida. Por favor, escribe un numero entero: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Ask the user to choose an operator
+                Console.WriteLine("Elige un operador de la siguiente lista:");
+                Console.WriteLine("\ta - Sumar");
+                Console.WriteLine("\ts - Restar");
+                Console.WriteLine("\tm - Multiplicar");
+                Console.WriteLine("\td - Dividir");
+                Console.WriteLine("\tc - Cuadrado del número mayor");
+                Console.Write("Que operación quieres realizar? ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("No es matemáticamente posible esta operación.\n");
+                    }
+                    else Console.WriteLine("Tu resultado: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! Ha ocurrido un error con la calculadora.\n - Detalles: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing
+                Console.Write("Pulsa 'n' e Intro para cerrar la aplicación, o pulsa otra tecla e Intro para continuar: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing
+            }
+            return;
+        }
+    }
+}
